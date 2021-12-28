76b1d97cfba0b3a842f3f51ae663e394376bf3e1
luisalfredopascualpolanco <61303559+luisalfredopascualpolanco@users.noreply.github.com>
2021-12-27 18:32:07 -0800

    version1.0

---
+++ Proyecto9.8/Proyecto9.8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto9._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int vuelta = 0;
+            int numero = 11;
+            while (vuelta < 25)
+            {
+                Console.WriteLine(numero);
+                numero = numero + 11;
+                vuelta++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
